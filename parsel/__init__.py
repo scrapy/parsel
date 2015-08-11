e52a729e012d77fd26ccdd3b0342f0bee5a2fd18
Elias Dorneles <eliasdorneles@gmail.com>
2015-08-11 15:03:27 -0300

    Bump version: 0.9.4 → 0.9.5

--- parsel/__init__.py
+++ parsel/__init__.py
@@ -5,6 +5,6 @@ or CSS selectors
 
 __author__ = 'Scrapy project'
 __email__ = 'info@scrapy.org'
-__version__ = '0.9.4'
+__version__ = '0.9.5'
 
 from parsel.selector import Selector, SelectorList  # NOQA
