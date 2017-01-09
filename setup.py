cde7689b726dd51c1c7e5c85beb56cfd56443cc1
Paul Tremberth <paul.tremberth@gmail.com>
2017-01-09 14:32:24 +0100

    Further update setup.py

--- setup.py
+++ setup.py
@@ -52,6 +52,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     setup_requires=['pytest-runner',],
     tests_require=['pytest',],
