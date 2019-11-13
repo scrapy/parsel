4d77c7136647708ae536303cd47fe12023ae8045
Adrián Chaves <adrian@chaves.io>
2019-11-13 09:31:36 +0100

    Install the sphinx-notfound-page Sphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,7 @@ import parsel
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
+    'notfound.extension',
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
