9de2cd29f8305927219c5daa3c1b97ad9c5c0d8e
Pascal Corpet <pascal@bayesimpact.org>
2021-07-27 17:26:36 +0200

    Move __all__ before imports to comply with PEP-8.
    See https://www.python.org/dev/peps/pep-0008/#module-level-dunder-names

--- parsel/__init__.py
+++ parsel/__init__.py
@@ -6,16 +6,15 @@ or CSS selectors
 __author__ = 'Scrapy project'
 __email__ = 'info@scrapy.org'
 __version__ = '1.6.0'
-
-from parsel.selector import Selector, SelectorList  # NOQA
-from parsel.csstranslator import css2xpath  # NOQA
-from parsel import xpathfuncs  # NOQA
-
 __all__ = [
     'Selector',
     'SelectorList',
     'css2xpath',
     'xpathfuncs',
 ]
+
+from parsel.selector import Selector, SelectorList  # NOQA
+from parsel.csstranslator import css2xpath  # NOQA
+from parsel import xpathfuncs  # NOQA
 
 xpathfuncs.setup()
