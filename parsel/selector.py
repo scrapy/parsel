64ae356d5288dfc5d58c621abe300a1bd2ef5d72
Andrey Rakhmatullin <wrar@wrar.name>
2022-11-01 16:12:36 +0600

    Document huge_tree.

--- parsel/selector.py
+++ parsel/selector.py
@@ -269,6 +269,14 @@ class Selector:
 
     ``base_url`` allows setting a URL for the document. This is needed when looking up external entities with relative paths.
     See the documentation for :func:`lxml.etree.fromstring` for more information.
+
+    ``huge_tree`` controls the lxml/libxml2 feature that forbids parsing
+    certain large documents to protect from possible memory exhaustion. The
+    argument is ``True`` by default if the installed lxml version supports it,
+    which disables the protection to allow parsing such documents. Set it to
+    ``False`` if you want to enable the protection.
+    See `this lxml FAQ entry <https://lxml.de/FAQ.html#is-lxml-vulnerable-to-xml-bombs>`_
+    for more information.
     """
 
     __slots__ = [
